61cbb0841a3df7e82f51195546103d38442f59bf
kashaf-sayab <kashafsayab21@gmail.com>
2024-08-16 12:11:10 +0500

    arrange files

--- includes/widgets.php
+++ includes/widgets.php
@@ -1,77 +1,94 @@
 <?php
-class WP_Book_Widget extends WP_Widget {
+function wp_book_load_widget() {
+    register_widget('WP_Book_Category_Widget');
+}
+add_action('widgets_init', 'wp_book_load_widget');
+
+class WP_Book_Category_Widget extends WP_Widget {
 
     public function __construct() {
         parent::__construct(
-            'wp_book_widget', // Base ID
-            'Books by Category', // Name
-            array('description' => __('A widget to display books by category', 'wp-book'))
+            'wp_book_category_widget', 
+            'Books by Category', 
+            array('description' => __('A Widget to display books by selected category.', 'wp-book'),) // Args
         );
     }
 
     public function widget($args, $instance) {
-        $category = !empty($instance['category']) ? $instance['category'] : '';
+        $category_id = !empty($instance['category_id']) ? $instance['category_id'] : 0;
+        
         echo $args['before_widget'];
         if (!empty($instance['title'])) {
             echo $args['before_title'] . apply_filters('widget_title', $instance['title']) . $args['after_title'];
         }
-        if ($category) {
-            $this->get_books_by_category($category);
-        }
+
+        $this->display_books_by_category($category_id);
+        
         echo $args['after_widget'];
     }
 
     public function form($instance) {
-        $title = !empty($instance['title']) ? $instance['title'] : '';
-        $category = !empty($instance['category']) ? $instance['category'] : '';
+        $title = !empty($instance['title']) ? $instance['title'] : __('Books by Category', 'wp-book');
+        $category_id = !empty($instance['category_id']) ? $instance['category_id'] : 0;
         ?>
         <p>
-            <label for="<?php echo esc_attr($this->get_field_id('title')); ?>"><?php _e('Title:', 'wp-book'); ?></label>
-            <input class="widefat" id="<?php echo esc_attr($this->get_field_id('title')); ?>" name="<?php echo esc_attr($this->get_field_name('title')); ?>" type="text" value="<?php echo esc_attr($title); ?>">
+            <label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:', 'wp-book'); ?></label>
+            <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo esc_attr($title); ?>">
         </p>
         <p>
-            <label for="<?php echo esc_attr($this->get_field_id('category')); ?>"><?php _e('Category:', 'wp-book'); ?></label>
-            <select class="widefat" id="<?php echo esc_attr($this->get_field_id('category')); ?>" name="<?php echo esc_attr($this->get_field_name('category')); ?>">
-                <?php
-                $terms = get_terms(array('taxonomy' => 'book_category', 'hide_empty' => false));
-                foreach ($terms as $term) {
-                    echo '<option value="' . esc_attr($term->slug) . '" ' . selected($category, $term->slug, false) . '>' . esc_html($term->name) . '</option>';
-                }
-                ?>
-            </select>
+            <label for="<?php echo $this->get_field_id('category_id'); ?>"><?php _e('Category:', 'wp-book'); ?></label>
+            <?php
+            wp_dropdown_categories(array(
+                'show_option_all' => __('Select Category', 'wp-book'),
+                'name' => $this->get_field_name('category_id'),
+                'id' => $this->get_field_id('category_id'),
+                'selected' => $category_id,
+                'taxonomy' => 'book_category',
+                'hide_empty' => false,
+            ));
+            ?>
         </p>
         <?php
     }
 
     public function update($new_instance, $old_instance) {
         $instance = array();
-        $instance['title'] = (!empty($new_instance['title'])) ? strip_tags($new_instance['title']) : '';
-        $instance['category'] = (!empty($new_instance['category'])) ? strip_tags($new_instance['category']) : '';
+        $instance['title'] = (!empty($new_instance['title'])) ? sanitize_text_field($new_instance['title']) : '';
+        $instance['category_id'] = (!empty($new_instance['category_id'])) ? intval($new_instance['category_id']) : 0;
         return $instance;
     }
 
-    private function get_books_by_category($category) {
-        $query = new WP_Query(array(
+    private function display_books_by_category($category_id) {
+        if (!$category_id) {
+            echo '<p>' . __('No category selected.', 'wp-book') . '</p>';
+            return;
+        }
+
+        $args = array(
             'post_type' => 'book',
             'tax_query' => array(
                 array(
                     'taxonomy' => 'book_category',
-                    'field'    => 'slug',
-                    'terms'    => $category,
+                    'field'    => 'id',
+                    'terms'    => $category_id,
                 ),
             ),
-        ));
+        );
 
-        if ($query->have_posts()) {
-            echo '<ul>';
-            while ($query->have_posts()) {
-                $query->the_post();
-                echo '<li><a href="' . get_permalink() . '">' . get_the_title() . '</a></li>';
-            }
-            echo '</ul>';
-        } else {
-            _e('No books found in this category.', 'wp-book');
+        $query = new WP_Query($args);
+
+        if (!$query->have_posts()) {
+            echo '<p>' . __('No books found in this category.', 'wp-book') . '</p>';
+            return;
         }
+
+        echo '<ul>';
+        while ($query->have_posts()) {
+            $query->the_post();
+            echo '<li><a href="' . get_permalink() . '">' . get_the_title() . '</a></li>';
+        }
+        echo '</ul>';
+
         wp_reset_postdata();
     }
 }
